4832af9af6b0b5f20050e449058e10edb8ea88cf
Daniella Lato <latodf@mcmaster.ca>
2020-06-29 14:18:09 -0400

    simple tests

--- Stats/gene_exp_and_inversions_stats.R
+++ Stats/gene_exp_and_inversions_stats.R
@@ -8,12 +8,6 @@ gei_dat <- read.csv("Sample_final_df.csv", header = TRUE)
 
 #Wilcox signed-rank test
 # to see if there is a difference btwn inversions and non-inverted segments
-gei_dat %>% select(norm_exp, inversion) %>% 
-  group_by(inversion) %>%
-  summarise(norm_exp = list(norm_exp)) %>% 
-  spread(inversion, norm_exp) %>%
-  mutate(p_value = t.test(unlist(0), unlist(1))$p.value,
-         t_value = t.test(unlist(0), unlist(1))$statistic)
-  
-  
-  wilcox.test(gei_dat$norm_exp, gei_dat$norm_exp, paired=TRUE)
+#null hyp = there is no difference in gene expression btwn inverted and non-inverted segments
+wilcox.test(gei_dat$norm_exp[gei_dat$inversion == 1], gei_dat$norm_exp[gei_dat$inversion == 0])
+t.test(gei_dat$norm_exp[gei_dat$inversion == 1], gei_dat$norm_exp[gei_dat$inversion == 0])
