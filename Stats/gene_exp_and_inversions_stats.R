d9113a9eb17b3a27587556228feb12041fd18ad1
Daniella Lato <latodf@mcmaster.ca>
2020-06-29 14:00:40 -0400

    wilcox and t-test

--- Stats/gene_exp_and_inversions_stats.R
+++ Stats/gene_exp_and_inversions_stats.R
@@ -1,4 +1,19 @@
 #test script for inversions and gene expression analysis
+library(tidyverse)
+library(dplyr)
+library(tidyr)
 
 #read in the data
 gei_dat <- read.csv("Sample_final_df.csv", header = TRUE)
+
+#Wilcox signed-rank test
+# to see if there is a difference btwn inversions and non-inverted segments
+gei_dat %>% select(norm_exp, inversion) %>% 
+  group_by(inversion) %>%
+  summarise(norm_exp = list(norm_exp)) %>% 
+  spread(inversion, norm_exp) %>%
+  mutate(p_value = t.test(unlist(0), unlist(1))$p.value,
+         t_value = t.test(unlist(0), unlist(1))$statistic)
+  
+  
+  wilcox.test(gei_dat$norm_exp, gei_dat$norm_exp, paired=TRUE)
