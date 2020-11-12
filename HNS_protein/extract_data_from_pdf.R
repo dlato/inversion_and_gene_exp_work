ba471e2946887c6d86b751f41d74444dd6d6da4c
Daniella Lato <latodf@mcmaster.ca>
2020-11-12 08:48:56 -0500

    attempting to instal chocolatey on laptop

--- HNS_protein/extract_data_from_pdf.R
+++ HNS_protein/extract_data_from_pdf.R
@@ -1,3 +1,6 @@
 #this will extract the data in tables from Oshima 2006 and Lang 2007 PDFs with H-NS binding data
 library(tabulizer)
 library(dplyr)
+
+
+tt4 <- tabulizer::extract_areas("raw_data_files/Oshima_2006_HNS_binding_sites_and_HGT.pdf")[[1]]
