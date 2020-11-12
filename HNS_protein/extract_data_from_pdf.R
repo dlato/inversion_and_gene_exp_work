58d9239fe16205247690da31c5684b15b8eac7bd
Daniella Lato <latodf@mcmaster.ca>
2020-11-12 10:52:16 -0500

    moving files and still trying to get data from pdf

--- HNS_protein/extract_data_from_pdf.R
+++ HNS_protein/extract_data_from_pdf.R
@@ -1,6 +1,12 @@
 #this will extract the data in tables from Oshima 2006 and Lang 2007 PDFs with H-NS binding data
 library(tabulizer)
 library(dplyr)
+library(readr) ## parse_number()
 
 
-tt4 <- tabulizer::extract_areas("raw_data_files/Oshima_2006_HNS_binding_sites_and_HGT.pdf")[[1]]
+#the number at the end of this next line indicates the single page to extract the information from
+#tt4 <- tabulizer::extract_areas("HNS_Protein/raw_data_files/Oshima_2006_HNS_binding_sites_and_HGT_supptab2.pdf")[[2]]
+o_tab <- extract_tables("HNS_Protein/raw_data_files/Oshima_2006_HNS_binding_sites_and_HGT_supptab2.pdf")
+final_o_tab <- do.call(rbind, o_tab[-length(o_tab)])
+
+m <- matrix(parse_number(c(tt4)), ncol=ncol(tt4))
