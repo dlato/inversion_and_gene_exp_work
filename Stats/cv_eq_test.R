a5eee08af89bdb509aec3e30bbc98a22980be972
Daniella Lato <latodf@mcmaster.ca>
2020-12-21 11:00:46 -0500

    exp dist

--- Stats/cv_eq_test.R
+++ Stats/cv_eq_test.R
@@ -3,6 +3,7 @@
 
 library(cvequality)
 library(dplyr)
+library(plyr)
 
 #read in dat
 g_dat <- read.table("Stats/inversions_gene_exp_wtest_data.csv", sep = "\t")
@@ -58,3 +59,48 @@ with(atcc_d,
      mslr_test(nr = 1e4, 
                norm_exp,
                rev_comp))
+
+
+# exp dist ALL BLOCKS
+#format data
+g_dat$inversion <- as.factor(g_dat$inversion)
+g_mu <- ddply(g_dat, "inversion", summarise, grp.mean=mean(norm_exp))
+p<- (ggplot(g_dat, aes(x=norm_exp, fill=inversion))
+     + geom_density(alpha=0.4)
+# Add mean lines
+     + geom_vline(data=g_mu, aes(xintercept=grp.mean, color=inversion),
+             linetype="dashed")
+#     + scale_x_continuous(trans = 'log10')
+   + labs(title= "Distribution of Gene Expression",x = "Gene Expression (CPM)", y = "Density") 
+   + scale_x_continuous(trans = 'log10', labels = function(x) ifelse(x ==0, "0", x))
+   + scale_y_continuous(labels = function(x) ifelse(x ==0,"0", x))
+   + scale_color_manual(values = c("#788AA3","#2E294E"), labels = c("Inversion","Non-Inversion"))
+   + scale_fill_manual(values = c("#788AA3","#2E294E"), labels = c("Inversion","Non-Inversion"))
+
+)
+pdf("Stats/all_exp_dist.pdf")
+p
+dev.off()
+
+# exp dist ATCC
+#format data
+atcc_d$rev_comp <- as.factor(atcc_d$rev_comp)
+summary(atcc_d)
+a_mu <- ddply(atcc_d, "rev_comp", summarise, grp.mean=mean(norm_exp))
+ecolT <- substitute(tit~italic(ecoli)~strain, list(tit = "Distribution of Gene Expression in", ecoli="E.coli",strain="ATCC 25922"))
+p<- (ggplot(atcc_d, aes(x=norm_exp, fill=rev_comp))
+     + geom_density(alpha=0.4)
+# Add mean lines
+     + geom_vline(data=a_mu, aes(xintercept=grp.mean, color=rev_comp),
+             linetype="dashed")
+#     + scale_x_continuous(trans = 'log10')
+   + labs(title= ecolT,x = "Gene Expression (CPM)", y = "Density") 
+   + scale_x_continuous(trans = 'log10', labels = function(x) ifelse(x ==0, "0", x))
+   + scale_y_continuous(labels = function(x) ifelse(x ==0,"0", x))
+   + scale_color_manual(values = c("#788AA3","#2E294E"), labels = c("Inversion","Non-Inversion"))
+   + scale_fill_manual(values = c("#788AA3","#2E294E"), labels = c("Inversion","Non-Inversion"))
+
+)
+pdf("Stats/ATCC_exp_dist.pdf")
+p
+dev.off()
